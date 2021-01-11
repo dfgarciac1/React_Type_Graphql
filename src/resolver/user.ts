ec46ef43b93424c97ec87f5ee03a1660c4299fd9
David Felipe G.C <ticaspaspi@gmail.com>
2021-01-11 18:59:18 -0500

    Funciona el login

--- src/resolver/user.ts
+++ src/resolver/user.ts
@@ -1,7 +1,7 @@
 import { TypeInfo } from "graphql";
 import { User } from "../entitys/User";
 import { Mycontext } from "src/types";
-import {Resolver ,Query, Ctx, Arg, Int, Mutation, InputType, Field} from "type-graphql"
+import {Resolver ,Query, Ctx, Arg, Int, Mutation, InputType, Field, ObjectType} from "type-graphql"
 import argon2 from "argon2"
 @InputType()
 class UsernamePasswordInput{
@@ -11,7 +11,22 @@ class UsernamePasswordInput{
  password:string
 }
 
+@ObjectType()
+class FieldError{
+@Field()
+field:string;
+@Field()
+message:string;
+}
 
+@ObjectType()
+class userResponse{
+    @Field(()=>[FieldError],{nullable:true})
+    errors?: FieldError[];
+    
+    @Field(()=>User,{nullable:true})
+    user?:User;
+}
 
 @Resolver()
 export class UserResolver{
@@ -24,5 +39,43 @@ export class UserResolver{
         const user = em.create(User,{username:options.username,password:hashedPassword})
         await em.persistAndFlush(user)
              return user;
+         }
+
+
+ @Query(()=>[User])
+ UserFind(
+@Ctx() {em}:Mycontext
+ ){
+     return em.find(User,{}) 
+ }
+
+ @Mutation(()=>userResponse)
+         async login(
+             @Arg("options") options: UsernamePasswordInput ,
+             @Ctx(){em}:Mycontext
+         ) : Promise<userResponse>{
+   const user = await em.findOne(User,{username:options.username}); 
+             if(!user ){
+                 return{
+                     errors: [
+                         {
+                            field:"username",
+                         message:'Ese usuario no existe'
+                        },
+                    ],
+                 };
+             }
+        const valid = await  argon2.verify(user.password,options.password)
+         if(!valid){
+            return{
+                errors: [
+                    {
+                       field:"password",
+                    message:'Contraseña incorrecta '
+                   },
+               ],
+            };
+         }
+             return {user};
          }
     }
